af04e9f6dc8a20e194ace48b24e1b1354c1e50c8
[DudiyMaksim] <[maksdudiya25@gmail.com]>
2024-01-21 21:58:00 +0200

    first commit

---
+++ Модуль 2. Змінні та типи даних.py
@@ -0,0 +1,9 @@
+# task 1
+a, b, c= map(int, input().split())
+print((a+b+c),(a*b*c))
+# task 2
+a, b, c= map(int, input().split())
+print(a-b-c)
+# task 3
+a, b= map(int, input().split())
+print(a*b/2)
